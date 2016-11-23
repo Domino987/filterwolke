730eca0ebcf9ff55411636a14762a753218ce41e
Steffen <steffen@engelit.de>
2016-11-23 16:00:25 +0100

    Init

---
+++ dots.js
@@ -0,0 +1,68 @@
+var dots = {
+    taz : {
+        x : 50,
+        y : 450,
+        title: "TAZ"
+    },
+    "Die Welt": {
+        x : 450,
+        y : 100,
+        title: "Die Welt"
+    },
+    FAZ : {
+        x: 400,
+        y : 150
+    },
+    Handelsblatt: {
+        x: 350,
+        y: 350
+    },
+    "Junge Welt": {
+        x: 50,
+        y: 310
+    },
+    "Neues Deutschland": {
+        x: 100,
+        y: 200
+    },
+    "Süddeutsche Zeitung": {
+        x: 100,
+        y: 500
+    },
+    "Junge Freiheit": {
+        x: 450,
+        y: 70
+    },
+    "Jungle World": {
+        x: 50,
+        y: 500
+    },
+    "National-Zeitung" : {
+        x:445,
+        y: 50
+    },
+    "Preußische Allgemeine Zeitung" : {
+        x:330,
+        y:150
+    },
+    "Der Spiegel": {
+        x: 150,
+        y: 470
+    },
+    "Focus": {
+            x: 330,
+            y: 280
+    },
+    "Stern": {
+        x: 290,
+        y: 320
+    },
+    "Deutsche Stimme": {
+        x: 295,
+        y: 10
+    },
+    graswurzelrevolution: {
+        x: 295,
+        y: 590
+    }
+};
